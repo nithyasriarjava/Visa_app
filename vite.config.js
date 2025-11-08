5184d7fff48e5165c79ff8f09183db7e8239f402
nithya sri <nithyasri.arjava.com>
2025-11-08 12:18:36 +0530

    done all changes

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  //  base: '/Visa_app/',
+   base: '/Visa_app/',
   build: {
     rollupOptions: {
       output: {
