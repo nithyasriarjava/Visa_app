d54b88f8e54d998821f740810bf9b631b33b79df
nithya sri <nithyasri.arjava.com>
2025-12-02 17:32:24 +0530

    new supabase added

--- src/components/supabaseClient.js
+++ src/components/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://wnvviathstqscskifolf.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndudnZpYXRoc3Rxc2Nza2lmb2xmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4ODA5OTIsImV4cCI6MjA3NDQ1Njk5Mn0.xsQ5hi8lL32hN0JdHLWA9pCXofCunJ5d4tMcAkqXm3g'
+const SUPABASE_URL = 'https://ifpbxwryoawbzjvvmcnn.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmcGJ4d3J5b2F3YnpqdnZtY25uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2NjA5NzYsImV4cCI6MjA4MDIzNjk3Nn0.YABc2UPEHOccV_LUKVdNKuiBt8dCM7IEmn_jYLgM1Kw'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
