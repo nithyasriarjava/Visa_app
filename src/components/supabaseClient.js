a4dafabbcc8196b271ff8368324ccf93437a866a
Mani <125241217+Mani-Arjava@users.noreply.github.com>
2025-12-04 14:29:44 +0530

    SupabaseClient file changes

--- src/components/supabaseClient.js
+++ src/components/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://rtjhwgiffujdqaskkqkp.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0amh3Z2lmZnVqZHFhc2trcWtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2NjA3NDMsImV4cCI6MjA4MDIzNjc0M30.GC2GVhbKDy0KtN7v1J6_kezR0U553ewgm44kizBhocM'
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
